b9b6e036278305a1f5d37cccadfec05412bc6e4a
ches85 <117942484+ches85@users.noreply.github.com>
2022-12-02 14:33:32 -0500

    Add files via upload

---
+++ Animal/Apple.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Apple here.
+ * Food for our elephant.
+ * 
+ * @author Brady 
+ * @version Nov 30 2022
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        setLocation(getX(), getY() + 1);
+    }    
+}
---
+++ Animal/Elephant.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * The Elephant, our hero.
+ * 
+ * @author Brady
+ * @version Nov 28 2022
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left)"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        // Remove apple if elephant eats it
+        removeTouching(Apple.class);
+    }    
+}
