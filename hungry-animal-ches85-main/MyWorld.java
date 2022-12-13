a9b542c1ebadb4842fd0c156ff88f8e811718fc9
ches85 <117942484+ches85@users.noreply.github.com>
2022-12-13 14:41:31 -0500

    Add files via upload

---
+++ hungry-animal-ches85-main/MyWorld.java
@@ -0,0 +1,67 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * The world the hero elephant lives in.
+ * 
+ * @author Brady
+ * @version Nov 11 2022
+ * 
+ */
+public class MyWorld extends World
+{
+    public int score = 0;
+    Label scoreLabel;
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1, false);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 20, 20);
+        
+        createApple();
+    }
+    
+    
+    /**
+     * End the game and draw 'Game Over'
+     */
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * Increase score
+     */
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen.
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
+    }
+    
+    
+}
+
